f84683c89b28b2b5655b8bf55ca5cc9320ea71f7
Octavia <98887468+iawthanb@users.noreply.github.com>
2024-08-21 17:22:45 +0300

    Update FocusMode.jsx

--- src/components/FocusMode.jsx
+++ src/components/FocusMode.jsx
@@ -6,7 +6,7 @@ const FocusMode = ({ onExit }) => {
   return (
     <div className="focus-mode d-flex flex-column justify-content-center align-items-center vh-100 bg-dark text-white">
       <h2 className="h1 fw-bold mb-3">Focus Mode</h2>
-      <p style="max-width: 400px;">Listen some live radio while keeping it productive!</p>
+      <p style={{max-width: 400px;}}>Listen some live radio while keeping it productive!</p>
       <audio controls autoPlay className="xot-audio-player">
         <source src={radioUrl} type="audio/mpeg" />
         Your browser does not support the audio element.
